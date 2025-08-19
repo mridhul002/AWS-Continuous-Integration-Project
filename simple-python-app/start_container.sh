3056fc267378d22dafedf3306255865cb00aff14
Mridhul Nair <159921075+mridhul002@users.noreply.github.com>
2025-08-19 20:32:17 +0530

    update files

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -2,6 +2,7 @@
  set -e
 
 # Pull the Docker image from Docker Hub
-
+docker pull mridhuldoc/simple-python-flask-app:latest
 
 # Run the Docker image as a container
+docker run -d -p 5000:5000 mridhuldoc/simple-python-flask-app
